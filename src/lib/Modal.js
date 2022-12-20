bf44a41b5341cc4209d60ac6fb00ddb7a7cf585c
Animasso Sidibe <asidibe017@gmail.com>
2022-12-20 14:44:49 +0100

    change

--- src/lib/Modal.js
+++ src/lib/Modal.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { useState } from "react";
 function Modal(props) {
-  const [modal, setModal] = useState(false);
-  const toggleModal = () => {
-    setModal(!modal);
-  };
   return (
     <>
       <div className="modal">
         <div className="modal-content">
           <h2>{props.textModal}</h2>
-          <button className="close-modal" onClick={toggleModal}>
+          <button className="close-modal" onClick={props.toggleModal}>
             Close
           </button>
         </div>
